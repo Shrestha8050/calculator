f49641ec13f4aaa580f33121ddea8d2e4fa5d9e9
Bodhan Shrestha <bodhanshrestha@gmail.com>
2020-08-16 11:02:31 +0545

    Updates

---
+++ precache-manifest.040a5d8218dc31ea5a485ce8182caa53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a27de97f7732eddcb8d068908cf73286",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "8acb7baf9aacec1d4ca3",
+    "url": "/calculator/static/css/main.c6414111.chunk.css"
+  },
+  {
+    "revision": "7f56221c83360223d760",
+    "url": "/calculator/static/js/2.25931f17.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator/static/js/2.25931f17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8acb7baf9aacec1d4ca3",
+    "url": "/calculator/static/js/main.101aa4de.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
